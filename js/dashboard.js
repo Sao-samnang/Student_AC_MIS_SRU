b153ecc93aaba6f9ffc90f7806571dcdc8ceaf50
Sao-samnang <149239046+Sao-samnang@users.noreply.github.com>
2024-12-29 08:42:42 +0700

    commit v--0.0.0.5.15
    test

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
     // menu toggle
+    // menuRespone();
+    // function menuRespone(){
+    //     $('#checkmenu').ready(function(){
+    //         if($(this).val()==0){ // Hide the menu
+    //             $(".menu-side nav ul li").css("width","auto"); 
+    //             $(".menu-side nav").css("width","180px");
+    //             $(".menu-side .btnlogout").css("width","180px");
+    //         }
+    //     })
+    // }
     $('#checkmenu').on('change', function() {
         if ($(this).is(':checked')) {
+            $(this).val(1);
             $(".menu-side").css("width","230px"); // Hide the menu
             $(".menu-side nav ul li").mouseover(function() {
                 $(this).css("background-color", "#4B088A"); 
@@ -13,7 +24,14 @@ $(document).ready(function(){
             }).mouseout(function() {
                 $(this).css("background-color", "#ffffff00"); 
             });
+            // 
+            // if($(this).val()==1){ // Hide the menu
+            //     $(".menu-side nav ul li").css("width","100%"); 
+            //     $(".menu-side nav").css("width","100%");
+            //     $(".menu-side .btnlogout").css("width","100%");
+            // }
         } else {
+            // $(this).val(0);
             $(".menu-side").css("width","55px"); // Show the menu
             $(".menu-side nav ul li").hover(function(){
                 $(this).css("background-color","#ffffff00");
